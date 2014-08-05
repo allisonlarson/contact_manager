7c45c6799bcf41e0f11073527e27cfc1e751e18b
Allison Larson <allisonlarson91@gmail.com>
2014-08-05 15:24:02 -0600

    email belongs to person

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,14 +1,19 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  let(:email_address) {EmailAddress.new(email: 'sample@example.com')}
+  let(:email_address) {EmailAddress.new(email: 'sample@example.com', person_id: 1)}
 
   it 'is valid' do
     expect(email_address).to be_valid
   end
 
-  it 'is not valid if empty' do
+  it 'is invalid if empty' do
     email_address.email = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'is invalid without person id' do
+    email_address.person_id = nil
     expect(email_address).to_not be_valid
   end
 end
