f2000607a721821ae63b1045cdd4196b8b18ab6d
Allison Larson <allisonlarson91@gmail.com>
2014-08-05 15:57:35 -0600

    show has edit email link

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -71,6 +71,10 @@ describe 'the person view', type: :feature do
     it 'has a list of email addresses' do
       expect(page).to have_selector('li', text: person.email_addresses.first.email)
     end
+
+    it 'has an add email address link' do
+      expect(page).to have_link('Add email address', href: new_email_address_path(person_id: person.id))
+    end
   end
 
 end
