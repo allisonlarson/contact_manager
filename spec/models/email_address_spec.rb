0fd8d64d37e466188783fb8eee930d869284b1c4
Allison Larson <allisonlarson91@gmail.com>
2014-08-05 15:20:35 -0600

    Email validation

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) {EmailAddress.new(email: 'sample@example.com')}
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is not valid if empty' do
+    email_address.email = nil
+    expect(email_address).to_not be_valid
+  end
 end
