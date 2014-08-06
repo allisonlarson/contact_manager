eb7caf990af5c666d1f142a27f65b670d6423817
Allison Larson <allisonlarson91@gmail.com>
2014-08-06 15:24:20 -0600

    allow for contact type between company and person

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -11,4 +11,13 @@ describe Company do
     walmart.name = nil
     expect(walmart).to_not be_valid
   end
+
+  it 'has an array of phone numbers' do
+    expect(walmart.phone_numbers).to eq([])
+  end
+
+  it "responds with its phone numbers after they're created" do
+    phone_number = walmart.phone_numbers.build(number: "333-4444")
+    expect(phone_number.number).to eq('333-4444')
+  end
 end
