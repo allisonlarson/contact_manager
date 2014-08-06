2688abde3d929268c14124477bd17eb5c807ba1d
Allison Larson <allisonlarson91@gmail.com>
2014-08-06 13:48:14 -0600

    Company is only valid with a name

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:walmart) {Company.create(name: 'Walmart')}
+
+  it 'is valid' do
+    expect(walmart).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    walmart.name = nil
+    expect(walmart).to_not be_valid
+  end
 end
